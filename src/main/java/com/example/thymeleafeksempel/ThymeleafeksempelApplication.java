6d0d101f298b17e3fff5361d03bcf12d4abb0d7f
OmarKayed <omar2346@stud.kea.dk>
2023-03-15 11:21:45 +0100

    Initial commit

---
+++ src/main/java/com/example/thymeleafeksempel/ThymeleafeksempelApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafeksempel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafeksempelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafeksempelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thymeleafeksempel/ThymeleafeksempelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafeksempel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafeksempelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
